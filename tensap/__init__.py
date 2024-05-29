98e6832be3c6c4362ddff5a6c584a80aafbb4548
alexandre-pasco <alexandre.pasco1702@gmail.com>
2024-05-30 00:26:34 +0200

    Added import cofnet

--- tensap/__init__.py
+++ tensap/__init__.py
@@ -108,6 +108,7 @@ from .approximation.tensor_approximation.principal_component_analysis.tensor_pri
 from .approximation.tensor_approximation.principal_component_analysis.functional_tensor_principal_component_analysis import (
     FunctionalTensorPrincipalComponentAnalysis,
 )
+from .cofnet.poincare_loss import *
 
 # Disable tensorflow's warning messages
 import os
