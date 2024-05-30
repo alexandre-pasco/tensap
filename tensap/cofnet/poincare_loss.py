09f7ae4b01d8c902592063bd92f98d2ce6aebd6d
alexandre-pasco <alexandre.pasco1702@gmail.com>
2024-05-30 13:01:04 +0200

    Improved and added operations with sigma and H.

--- tensap/cofnet/poincare_loss.py
+++ tensap/cofnet/poincare_loss.py
@@ -1,5 +1,7 @@
 import numpy as np
-from scipy.sparse.linalg import LinearOperator
+import scipy
+
+
 
 
 class PoincareLossFunction():
@@ -13,7 +15,7 @@ class PoincareLossFunction():
 
     """
 
-    def __init__(self, jac_u, jac_g):
+    def __init__(self, jac_u=None, jac_g=None):
         """
         Constructor for the class PoincareLossFunction.
 
@@ -26,17 +28,18 @@ class PoincareLossFunction():
         self.jac_g = jac_g
 
     @staticmethod
-    def _eval(jac_g, jac_u):
+    def _eval(jac_u, jac_g):
         jac_ut = np.moveaxis(jac_u, -1, -2)
         proj = np.array([jg.T @ np.linalg.solve(jg @ jg.T, jg) for jg in jac_g])
-        residual = jac_ut - np.einsum("kil,klj", proj, jac_ut, optimize=True)
-        loss = np.linalg.norm(residual, axis=(1,2)) ** 2
+        residual = jac_ut - np.einsum("kil,klj->kij", proj, jac_ut, optimize=True)
+        losses = np.linalg.norm(residual, axis=(1,2)) ** 2
+        loss = losses.mean()
         return loss
 
     def eval(self, jac_u=None, jac_g=None):
         if jac_u is None : jac_u = self.jac_u
         if jac_g is None : jac_g = self.jac_g
-        return self._eval(self.jac_g, self.jac_u)
+        return self._eval(self.jac_u, self.jac_g)
 
 
 class RayleighPoincareLossFunction():
@@ -55,7 +58,7 @@ class RayleighPoincareLossFunction():
     def _eval(G, jac_u, jac_basis):
         jac_g = np.einsum("ij, kjl", G.T, jac_basis)
         jac_g = np.moveaxis(jac_g, 0, 1)
-        return PoincareLossFunction._eval(jac_g, jac_u)
+        return PoincareLossFunction._eval(jac_u, jac_g)
 
     def eval(self, G, jac_u=None, jac_basis=None):
         if jac_u is None: jac_u = self.jac_u
@@ -74,7 +77,7 @@ class RayleighPoincareLossFunction():
     @staticmethod
     def _eval_gradients(G, jac_u, jac_basis):
         m = G.shape[1]
-        d = jac_u.shape[1]
+        d = jac_u.shape[2]
         N = len(jac_u)
         jac_g = np.moveaxis(np.einsum("ij, kjl", G.T, jac_basis), 0, 1)
         gradients = np.zeros((N,) + G.shape)
@@ -89,7 +92,7 @@ class RayleighPoincareLossFunction():
     def eval_gradients(self, G, jac_u=None, jac_basis=None):
         if jac_u is None: jac_u = self.jac_u
         if jac_basis is None: jac_basis = self.jac_basis
-        return self.eval_gradients(G, jac_u, jac_basis)
+        return self._eval_gradients(G, jac_u, jac_basis)
 
     @staticmethod
     def _apply_s_h(X, G, jac_u, jac_basis):
@@ -101,13 +104,16 @@ class RayleighPoincareLossFunction():
         Hx = np.zeros(Xmat.shape)
         Sx = np.zeros(Xmat.shape)
         for jb, ju in zip(jac_basis, jac_u):
-            B = jb @ jb.T
-            A = jb @ ju.T @ ju @ jb.T
-            GBG = G.T @ B @ G
-            GAG = G.T @ A @ G
+            jbju = jb @ ju.T
+            jg = G.T @ jb
+            jgju = jg @ ju.T
+            # B = jb @ jb.T
+            # A = jb @ ju.T @ ju @ jb.T
+            GBG = jg @ jg.T
+            GAG = jgju @ jgju.T
             Y = np.linalg.solve(GBG.T, Xmat.T)
-            Hx += A @ Y.T / N
-            Sx += B @ np.linalg.solve(GBG.T, GAG.T @ Y).T / N
+            Hx += jbju @ jbju.T @ Y.T / N
+            Sx += jb @ jb.T @ np.linalg.solve(GBG.T, GAG.T @ Y).T / N
 
         if X.ndim == 1:
             Sx = Sx.flatten(order='F') # column-wise ordering
@@ -125,8 +131,8 @@ class RayleighPoincareLossFunction():
         Km =  G.shape[0] * G.shape[1]
         matvec_s = lambda X : RayleighPoincareLossFunction._apply_s_h(X, G, jac_u, jac_basis)[0]
         matvec_h = lambda X: RayleighPoincareLossFunction._apply_s_h(X, G, jac_u, jac_basis)[1]
-        S = LinearOperator((Km, Km), matvec=matvec_s)
-        H = LinearOperator((Km, Km), matvec=matvec_h)
+        S = scipy.sparse.linalg.LinearOperator((Km, Km), matvec=matvec_s)
+        H = scipy.sparse.linalg.LinearOperator((Km, Km), matvec=matvec_h)
         return S, H
 
     def build_operator_s_h(self, G, jac_u=None, jac_basis=None):
@@ -154,3 +160,45 @@ class RayleighPoincareLossFunction():
         if jac_u is None: jac_u = self.jac_u
         if jac_basis is None: jac_basis = self.jac_basis
         return self._build_diag_s(G, jac_u, jac_basis)
+
+    @staticmethod
+    def _apply_inv_s(X, G, jac_u, jac_basis, **kwargs):
+        sigma = RayleighPoincareLossFunction._build_operator_s_h(G, jac_u, jac_basis)[0]
+        diag = RayleighPoincareLossFunction._build_diag_s(G, jac_u, jac_basis)
+        M = scipy.sparse.diags(1/diag)
+        out = scipy.sparse.linalg.cg(sigma, X, M=M, **kwargs)[0]
+        return out
+
+    def apply_inv_s(self, X, G, jac_u=None, jac_basis=None, **kwargs):
+        if jac_u is None: jac_u = self.jac_u
+        if jac_basis is None: jac_basis = self.jac_basis
+        return self._apply_inv_s(X, G, jac_u, jac_basis, **kwargs)
+
+    @staticmethod
+    def _build_full_s(G, jac_u, jac_basis):
+        Km = G.shape[0] * G.shape[1]
+        N = jac_u.shape[0]
+        S = np.zeros((Km, Km))
+        for ju, jb in zip(jac_u, jac_basis):
+            B = jb @ jb.T
+            jg = G.T @ jb
+            jgju = jg @ ju.T
+            GBG = jg @ jg.T
+            GAG = jgju @ jgju.T
+            GBG_inv = np.linalg.inv(GBG)
+            S += np.kron(GBG_inv @ GAG @ GBG_inv, B) / N
+        return S
+
+    @staticmethod
+    def _build_full_h(G, jac_u, jac_basis):
+        Km = G.shape[0] * G.shape[1]
+        N = jac_u.shape[0]
+        H = np.zeros((Km, Km))
+        for ju, jb in zip(jac_u, jac_basis):
+            B = jb @ jb.T
+            jbju = jb @ ju.T
+            A = jbju @ jbju.T
+            GBG = G.T @ jb @ jb.T @ G
+            GBG_inv = np.linalg.inv(GBG)
+            H += np.kron(GBG_inv, A) / N
+        return H
