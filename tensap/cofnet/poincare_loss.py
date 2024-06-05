f8adf52893b5e887d8270660effe864f9ac0a570
alexandre-pasco <alexandre.pasco1702@gmail.com>
2024-06-05 15:09:43 +0200

    Added methods with Hessian. Removed methods returning LinearOperator.

--- tensap/cofnet/poincare_loss.py
+++ tensap/cofnet/poincare_loss.py
@@ -181,9 +181,11 @@ class RayleighPoincareLossFunction:
         return self._eval_gradient(G, jac_u, jac_basis)
 
     @staticmethod
-    def _apply_s_h(X, G, jac_u, jac_basis):
+    def _apply_sigma_h(X, G, jac_u, jac_basis):
         """
-        ...
+        Compute the matrix-vector multiplication Sigma(G)X and H(G)X as described in Bigoni et al. 2022, where
+        X is a column-major vectorized matrix of same size as G.
+        In other words, it computes Sigma(G) @ X.flatten('F') and H(G) @ X.flatten('F').
 
         Parameters
         ----------
@@ -204,7 +206,7 @@ class RayleighPoincareLossFunction:
             Has same shape as X.
         """
         if X.ndim == 1:
-            Xmat = X.reshape(G.shape, order='F')  # column-wise ordering
+            Xmat = X.reshape(G.shape, order='F')  # column-major ordering
         else:
             Xmat = X
         N = len(jac_u)
@@ -214,23 +216,22 @@ class RayleighPoincareLossFunction:
             jbju = jb @ ju.T
             jg = G.T @ jb
             jgju = jg @ ju.T
-            # B = jb @ jb.T
-            # A = jb @ ju.T @ ju @ jb.T
             GBG = jg @ jg.T
             GAG = jgju @ jgju.T
             Y = np.linalg.solve(GBG.T, Xmat.T)
             Hx += jbju @ jbju.T @ Y.T / N
             Sx += jb @ jb.T @ np.linalg.solve(GBG.T, GAG.T @ Y).T / N
 
-        if X.ndim == 1:
-            Sx = Sx.flatten(order='F')  # column-wise ordering
-            Hx = Hx.flatten(order='F')
-
+        Sx = Sx.reshape(X.shape, order='F')  # column-major ordering
+        Hx = Hx.reshape(X.shape, order='F')
         return Sx, Hx
 
-    def apply_s_h(self, X, G, jac_u=None, jac_basis=None):
+    def apply_sigma_h(self, X, G, jac_u=None, jac_basis=None):
         """
-        ...
+        Compute the matrix-vector multiplication Sigma(G)X and H(G)X as described in Bigoni et al. 2022, where
+        X is a column-major vectorized matrix of same size as G.
+        In other words, it computes Sigma(G) @ X.flatten('F') and H(G) @ X.flatten('F').
+        If optional parameters are not provided, then the corresponding attributes are used.
 
         Parameters
         ----------
@@ -238,9 +239,9 @@ class RayleighPoincareLossFunction:
             Has shape (K, m) or (K*m, ).
         G : numpy.ndarray
             Has shape (K, m).
-        jac_u : numpy.ndarray
+        jac_u : numpy.ndarray, optional.
             Has shape (N, n, d)
-        jac_basis : numpy.ndarray
+        jac_basis : numpy.ndarray, optional.
             Has shape (N, K, d)
 
         Returns
@@ -252,74 +253,16 @@ class RayleighPoincareLossFunction:
         """
         if jac_u is None: jac_u = self.jac_u
         if jac_basis is None: jac_basis = self.jac_basis
-        return self._apply_s_h(X, G, jac_u, jac_basis)
+        return self._apply_sigma_h(X, G, jac_u, jac_basis)
 
     @staticmethod
-    def _build_operator_s_h(G, jac_u, jac_basis):
+    def _eval_sigma_diag(G, jac_u, jac_basis):
         """
-        ...
+        Compute the diagnoal of the matrix Sigma(G) from Bigoni et al. 2022.
+        It is used for preconditioning the conjugate gradient used to apply the inverse of Sigma(G).
 
         Parameters
         ----------
-        X : numpy.ndarray
-            Has shape (K, m) or (K*m, ).
-        G : numpy.ndarray
-            Has shape (K, m).
-        jac_u : numpy.ndarray
-            Has shape (N, n, d)
-        jac_basis : numpy.ndarray
-            Has shape (N, K, d)
-
-        Returns
-        -------
-        S : scipy.sparse.linalg.LinearOperator
-            Has shape (K*m, K*m).
-        H : scipy.sparse.linalg.LinearOperator
-            Has shape (K*m, K*m).
-        """
-        Km =  G.shape[0] * G.shape[1]
-        matvec_s = lambda X : RayleighPoincareLossFunction._apply_s_h(X, G, jac_u, jac_basis)[0]
-        matvec_h = lambda X: RayleighPoincareLossFunction._apply_s_h(X, G, jac_u, jac_basis)[1]
-        S = scipy.sparse.linalg.LinearOperator((Km, Km), matvec=matvec_s)
-        H = scipy.sparse.linalg.LinearOperator((Km, Km), matvec=matvec_h)
-        return S, H
-
-    def build_operator_s_h(self, G, jac_u=None, jac_basis=None):
-        """
-        ...
-
-        Parameters
-        ----------
-        X : numpy.ndarray
-            Has shape (K, m) or (K*m, ).
-        G : numpy.ndarray
-            Has shape (K, m).
-        jac_u : numpy.ndarray
-            Has shape (N, n, d)
-        jac_basis : numpy.ndarray
-            Has shape (N, K, d)
-
-        Returns
-        -------
-        S : scipy.sparse.linalg.LinearOperator
-            Has shape (K*m, K*m).
-        H : scipy.sparse.linalg.LinearOperator
-            Has shape (K*m, K*m).
-        """
-        if jac_u is None: jac_u = self.jac_u
-        if jac_basis is None: jac_basis = self.jac_basis
-        return self._build_operator_s_h(G, jac_u, jac_basis)
-
-    @staticmethod
-    def _build_diag_s(G, jac_u, jac_basis):
-        """
-        ...
-        Compute the diagnoal of the matrix Sigma from Bigoni et al. 2022.
-
-        Parameters
-        ----------
-        X : numpy.ndarray
-            Has shape (K, m) or (K*m, ).
         G : numpy.ndarray
             Has shape (K, m).
         jac_u : numpy.ndarray
@@ -346,15 +289,14 @@ class RayleighPoincareLossFunction:
             diag += np.kron(diag1, diag2) / N
         return diag
 
-    def build_diag_s(self, G, jac_u=None, jac_basis=None):
+    def eval_sigma_diag(self, G, jac_u=None, jac_basis=None):
         """
-        ...
-        Compute the diagnoal of the matrix Sigma from Bigoni et al. 2022.
+        Compute the diagnoal of the matrix Sigma(G) from Bigoni et al. 2022.
+        It is used for preconditioning the conjugate gradient used to apply the inverse of Sigma(G).
+        If optional parameters are not provided, then the corresponding attributes are used.
 
         Parameters
         ----------
-        X : numpy.ndarray
-            Has shape (K, m) or (K*m, ).
         G : numpy.ndarray
             Has shape (K, m).
         jac_u : numpy.ndarray
@@ -369,13 +311,13 @@ class RayleighPoincareLossFunction:
         """
         if jac_u is None: jac_u = self.jac_u
         if jac_basis is None: jac_basis = self.jac_basis
-        return self._build_diag_s(G, jac_u, jac_basis)
+        return self._eval_sigma_diag(G, jac_u, jac_basis)
 
     @staticmethod
-    def _apply_inv_s(X, G, jac_u, jac_basis, **kwargs):
+    def _apply_sigma_inv(X, G, jac_u, jac_basis, **kwargs):
         """
-        ...
-        Compute the diagnoal of the matrix Sigma from Bigoni et al. 2022.
+        Apply the inverse of the matrix Sigma(G) from Bigoni et al. 2022 to a vector X.
+        The conjugate gradient method is used with Jacobi preconditioning.
 
         Parameters
         ----------
@@ -387,28 +329,60 @@ class RayleighPoincareLossFunction:
             Has shape (N, n, d)
         jac_basis : numpy.ndarray
             Has shape (N, K, d)
+        **kwargs : dict
+            Key word arguments for scipy.sparse.linalg.cg.
 
         Returns
         -------
-        diag : numpy.ndarray
-            Has shape (K*m, ).
+        out : numpy.ndarray
+            Has same shape as X.
         """
-        #TODO when G is flattened or not
-        sigma = RayleighPoincareLossFunction._build_operator_s_h(G, jac_u, jac_basis)[0]
-        diag = RayleighPoincareLossFunction._build_diag_s(G, jac_u, jac_basis)
+        if X.ndim == 2:
+            Xvec = X.flatten(order='F')  # column-major flattening
+        else:
+            Xvec = X
+        Km = G.shape[0] * G.shape[1]
+        matvec = lambda X: RayleighPoincareLossFunction._apply_sigma_h(X, G, jac_u, jac_basis)[0]
+        sigma = scipy.sparse.linalg.LinearOperator((Km, Km), matvec=matvec)
+        diag = RayleighPoincareLossFunction._eval_sigma_diag(G, jac_u, jac_basis)
         M = scipy.sparse.diags(1/diag)
-        out = scipy.sparse.linalg.cg(sigma, X, M=M, **kwargs)[0]
+        out, info = scipy.sparse.linalg.cg(sigma, Xvec, M=M, **kwargs)
+        out = out.reshape(X.shape, order='F')
         return out
 
-    def apply_inv_s(self, X, G, jac_u=None, jac_basis=None, **kwargs):
+    def apply_sigma_inv(self, X, G, jac_u=None, jac_basis=None, **kwargs):
+        """
+        Apply the inverse of the matrix Sigma(G) from Bigoni et al. 2022 to a vector X.
+        The conjugate gradient method is used with Jacobi preconditioning.
+        If optional parameters are not provided, then the corresponding attributes are used.
+
+        Parameters
+        ----------
+        X : numpy.ndarray
+            Has shape (K, m) or (K*m, ).
+        G : numpy.ndarray
+            Has shape (K, m).
+        jac_u : numpy.ndarray
+            Has shape (N, n, d)
+        jac_basis : numpy.ndarray
+            Has shape (N, K, d)
+        **kwargs : dict
+            Key word arguments for scipy.sparse.linalg.cg.
+
+        Returns
+        -------
+        out : numpy.ndarray
+            Has same shape as X.
+        """
         if jac_u is None: jac_u = self.jac_u
         if jac_basis is None: jac_basis = self.jac_basis
-        return self._apply_inv_s(X, G, jac_u, jac_basis, **kwargs)
+        return self._apply_sigma_inv(X, G, jac_u, jac_basis, **kwargs)
 
     @staticmethod
-    def _build_full_s_h(G, jac_u, jac_basis):
+    def _eval_sigma_h_full(G, jac_u, jac_basis):
         """
-        ...
+        Build the full matrices Sigma(G) and H(G) from Bigoni et al. 2022.
+        This should only be used for debugging or testing purpose.
 
         Parameters
         ----------
@@ -443,9 +417,107 @@ class RayleighPoincareLossFunction:
             H += np.kron(GBG_inv, A) / N
         return S, H
 
-    def build_full_s_h(self, G, jac_u=None, jac_basis=None):
+    def eval_sigma_h_full(self, G, jac_u=None, jac_basis=None):
         """
-        ...
+        Build the full matrices Sigma(G) and H(G) from Bigoni et al. 2022.
+        This should only be used for debugging or testing purpose.
+        If optional parameters are not provided, then the corresponding attributes are used.
+
+        Parameters
+        ----------
+        G : numpy.ndarray
+            Has shape (K, m).
+        jac_u : numpy.ndarray, optional
+            Has shape (N, n, d)
+        jac_basis : numpy.ndarray, optional
+            Has shape (N, K, d)
+
+        Returns
+        -------
+        S : numpy.ndarray
+            Has shape (K*m, K*m).
+        H : numpy.ndarray
+            Has shape (K*m, K*m).
+        """
+        if jac_u is None: jac_u = self.jac_u
+        if jac_basis is None: jac_basis = self.jac_basis
+        return self._eval_sigma_h_full(G, jac_u, jac_basis)
+
+    @staticmethod
+    def _apply_hessian(X, G, jac_u, jac_basis):
+        """
+        Compute the matrix-vector multiplication Hess(G) @ X where
+        X is a column-major vectorized matrix of same size as G.
+        In other words, it computes Hess(G) @ X.flatten('F').
+
+        Parameters
+        ----------
+        X : numpy.ndarray
+            Has shape (K, m) or (K*m, ).
+        G : numpy.ndarray
+            Has shape (K, m).
+        jac_u : numpy.ndarray, optional.
+            Has shape (N, n, d)
+        jac_basis : numpy.ndarray, optional.
+            Has shape (N, K, d)
+
+        Returns
+        -------
+        hess : numpy.ndarray
+            Has same shape as X.
+        """
+        if X.ndim == 1:
+            Xmat = X.reshape(G.shape, order='F')  # column-major ordering
+        else:
+            Xmat = X
+        d = jac_u.shape[-1]
+        N = len(jac_u)
+        hess = 0 * Xmat
+        for jb, ju in zip(jac_basis, jac_u):
+            grad_u = ju.T
+            djg = Xmat.T @ jb
+            jg = G.T @ jb
+            jg_inv = scipy.linalg.pinv(jg)
+            p = np.eye(d) - jg_inv @ jg
+            res1 = 2 * jg_inv.T @ (djg.T @ jg_inv.T - jg_inv @ djg @ p) @ grad_u @ grad_u.T @ p
+            res2 = 2 * jg_inv.T @ grad_u @ grad_u.T @ (jg_inv @ djg @ p + p @ djg.T @ jg_inv.T)
+            hess += jb @ (res1 + res2).T / N
+
+        hess = hess.reshape(X.shape, order='F')  # column-major ordering
+        return hess
+
+    def apply_hessian(self, X, G, jac_u=None, jac_basis=None):
+        """
+        Compute the matrix-vector multiplication Hess(G) @ X where
+        X is a column-major vectorized matrix of same size as G.
+        In other words, it computes Hess(G) @ X.flatten('F').
+
+        Parameters
+        ----------
+        X : numpy.ndarray
+            Has shape (K, m) or (K*m, ).
+        G : numpy.ndarray
+            Has shape (K, m).
+        jac_u : numpy.ndarray, optional.
+            Has shape (N, n, d)
+        jac_basis : numpy.ndarray, optional.
+            Has shape (N, K, d)
+
+        Returns
+        -------
+        hess : numpy.ndarray
+            Has same shape as X.
+        """
+        if jac_u is None: jac_u = self.jac_u
+        if jac_basis is None: jac_basis = self.jac_basis
+        return self._apply_hessian(X, G, jac_u, jac_basis)
+
+    @staticmethod
+    def _eval_hessian_diag(G, jac_u, jac_basis):
+        """
+        Compute the diagnoal of the matrix Hess(G).
+        It is used for preconditioning the conjugate gradient used to apply the inverse of Hess(G).
+        If optional parameters are not provided, then the corresponding attributes are used.
 
         Parameters
         ----------
@@ -458,11 +530,110 @@ class RayleighPoincareLossFunction:
 
         Returns
         -------
-        S : numpy.ndarray
-            Has shape (K*m, K*m).
-        H : numpy.ndarray
-            Has shape (K*m, K*m).
+        diag : numpy.ndarray
+            Has shape (K*m, ).
+        """
+        d = jac_u.shape[-1]
+        N = len(jac_u)
+        diag_mat = 0 * G
+        for jb, ju in zip(jac_basis, jac_u):
+            grad_u = ju.T
+            jg = G.T @ jb
+            jg_inv = scipy.linalg.pinv(jg)
+            p = np.eye(jb.shape[1]) - jg_inv @ jg
+            diag_mat += (jb @ p @ grad_u @ grad_u.T @ jg_inv) * (jb @ jg_inv)
+            diag_mat -= (jb @ p @ grad_u) ** 2 @ np.diag(jg_inv.T @ jg_inv).reshape(1, -1)
+            diag_mat += (jb @ jg_inv) * (jb @ p @ grad_u @ grad_u.T @ jg_inv)
+            diag_mat += (jb @ p) ** 2 @ np.ones((d, 1)) @ np.diag(jg_inv.T @ grad_u @ grad_u.T @ jg_inv).reshape(1, -1)
+        diag_mat = 2 * diag_mat / N
+        diag = diag_mat.flatten(order="F")
+        return diag
+
+    def eval_hessian_diag(self, G, jac_u=None, jac_basis=None):
+        """
+        Compute the diagnoal of the matrix Hess(G).
+        It is used for preconditioning the conjugate gradient used to apply the inverse of Hess(G).
+        If optional parameters are not provided, then the corresponding attributes are used.
+
+        Parameters
+        ----------
+        G : numpy.ndarray
+            Has shape (K, m).
+        jac_u : numpy.ndarray
+            Has shape (N, n, d)
+        jac_basis : numpy.ndarray
+            Has shape (N, K, d)
+
+        Returns
+        -------
+        diag : numpy.ndarray
+            Has shape (K*m, ).
         """
         if jac_u is None: jac_u = self.jac_u
         if jac_basis is None: jac_basis = self.jac_basis
-        return self._build_full_s_h(G, jac_u, jac_basis)
+        return self._eval_hessian_diag(G, jac_u, jac_basis)
+
+    @staticmethod
+    def _apply_hessian_inv(X, G, jac_u, jac_basis, **kwargs):
+        """
+        Apply the inverse of the matrix Hess(G) to a vector X.
+        The conjugate gradient method is used with Jacobi preconditioning.
+
+        Parameters
+        ----------
+        X : numpy.ndarray
+            Has shape (K, m) or (K*m, ).
+        G : numpy.ndarray
+            Has shape (K, m).
+        jac_u : numpy.ndarray
+            Has shape (N, n, d)
+        jac_basis : numpy.ndarray
+            Has shape (N, K, d)
+        **kwargs : dict
+            Key word arguments for scipy.sparse.linalg.cg.
+
+        Returns
+        -------
+        out : numpy.ndarray
+            Has same shape as X.
+        """
+        if X.ndim == 2:
+            Xvec = X.flatten(order='F')  # column-major flattening
+        else:
+            Xvec = X
+        Km = G.shape[0] * G.shape[1]
+        matvec = lambda X: RayleighPoincareLossFunction._apply_hessian(X, G, jac_u, jac_basis)
+        hess = scipy.sparse.linalg.LinearOperator((Km, Km), matvec=matvec)
+        diag = RayleighPoincareLossFunction._eval_hessian_diag(G, jac_u, jac_basis)
+        M = scipy.sparse.diags(1 / diag)
+        out, info = scipy.sparse.linalg.cg(hess, Xvec, M=M, **kwargs)
+        out = out.reshape(X.shape, order='F')
+        return out
+
+    def apply_hessian_inv(self, X, G, jac_u=None, jac_basis=None, **kwargs):
+        """
+        Apply the inverse of the matrix Hess(G) to a vector X.
+        The conjugate gradient method is used with Jacobi preconditioning.
+
+        Parameters
+        ----------
+        X : numpy.ndarray
+            Has shape (K, m) or (K*m, ).
+        G : numpy.ndarray
+            Has shape (K, m).
+        jac_u : numpy.ndarray
+            Has shape (N, n, d)
+        jac_basis : numpy.ndarray
+            Has shape (N, K, d)
+        **kwargs : dict
+            Key word arguments for scipy.sparse.linalg.cg.
+
+        Returns
+        -------
+        out : numpy.ndarray
+            Has same shape as X.
+        """
+        if jac_u is None: jac_u = self.jac_u
+        if jac_basis is None: jac_basis = self.jac_basis
+        return self._apply_hessian_inv(X, G, jac_u, jac_basis, **kwargs)
+    
