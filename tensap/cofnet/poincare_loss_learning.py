62c1b9a25abe95bdb44bf9d7ca9299c9f90f0431
alexandre-pasco <alexandre.pasco1702@gmail.com>
2024-07-03 00:30:04 +0200

    Poincare learning with QN, scipy and pymanopt. Associated unitary tests.

---
+++ tensap/cofnet/poincare_loss_learning.py
@@ -0,0 +1,97 @@
+import numpy as np
+import scipy
+from tensap.cofnet.poincare_loss_bigoni import *
+
+
+def iteration_qn_bigoni(G, jac_u, jac_basis, **kwargs):
+    N, K, d = jac_basis.shape
+    Gmat = G.reshape(K, -1, order='F')
+    jac_g = eval_jac_g(Gmat, jac_basis)
+    b = eval_HG_X(Gmat, Gmat, jac_u, jac_basis, jac_g)
+    Gaux = eval_SGinv_X(Gmat, b, jac_u, jac_basis, jac_g, **kwargs)
+    Gnext = np.linalg.svd(Gaux, full_matrices=False)[0]  # orthonormalize
+    # M = Gaux.T @ R @ Gaux  # orthonormalization
+    # Mcholinv = scipy.linalg.inv(scipy.linalg.cholesky(M))
+    # Gnext = Gaux @ Mcholinv
+    return Gnext
+
+def minimize_qn_bigoni(G0, jac_u, jac_basis, maxiter_qn=100, tol_qn=1e-5, verbosity=2, **kwargs):
+    N, K, d = jac_basis.shape
+    G0mat = G0.reshape(K, -1, order='F')
+    Gnow = G0mat
+    i = -1
+    delta = np.inf
+    if verbosity >= 1:
+        print("Optimizing Poincare loss with QN from Bigoni et al.")
+    while i < maxiter_qn and delta >= tol_qn:
+        i = i+1
+        Gnext = iteration_qn_bigoni(Gnow, jac_u, jac_basis, **kwargs)
+        delta = np.linalg.norm(Gnext - Gnow)
+        # delta = 1 - np.linalg.svd(Gnext.T @ Gnow)[1].min()
+        Gnow = Gnext
+        if verbosity >= 2:
+            err = poincare_loss_bigoni(Gnow, jac_u, jac_basis)
+            print(f"| Iter:{i} loss:{err:.3e} step_size:{delta:.3e}")
+    return Gnow
+
+def minimize_scipy(G0, jac_u, jac_basis, verbosity=1, **kwargs):
+    N, K, d = jac_basis.shape
+    G0vec = G0.flatten(order='F')  # column-major ordering
+
+    # functions used in scipy.optimize.minimize
+    def loss(x):
+        return poincare_loss_bigoni(x, jac_u, jac_basis)
+    def loss_grad(x):
+        return poincare_loss_bigoni_gradient(x, jac_u, jac_basis)
+    def hessp(x, p):
+        return eval_SG_X(x, p, jac_u, jac_basis)
+    def orthonormalize(x):
+        xmat = np.linalg.svd(x.reshape(G0.shape), full_matrices=False)[0]
+        x[:] = xmat.flatten("F")
+
+    def print_info(x):
+        l = loss(x)
+        dl = np.linalg.norm(loss_grad(x))
+        print(f"| loss_train={l:.3e} | norm_grad_loss_train={dl:.3e}")
+
+    def callback(x):
+        orthonormalize(x)
+        if verbosity >= 1:
+            print_info(x)
+
+    res = scipy.optimize.minimize(fun=loss, x0=G0vec, jac=loss_grad, hessp=hessp, callback=callback, **kwargs)
+    Gfit = res.x.reshape(G0.shape)
+    return Gfit
+
+def minimize_pymanopt(G0, jac_u, jac_basis, use_precond=True, pmo_kwargs={}, precond_kwargs={}):
+    K = jac_basis.shape[1]
+    if G0.ndim == 1:
+        G0mat = G0.Reshape((K, G0.shape[0] // K))
+    else:
+        G0mat = G0
+    m = G0mat.shape[1]
+    problem, optimizer = build_pymanopt_problem(jac_u, jac_basis, m, use_precond, pmo_kwargs, precond_kwargs)
+    optim_result = optimizer.run(problem, initial_point=G0)
+    result = optim_result.point
+    return result
+
+def build_pymanopt_problem(jac_u, jac_basis, m, use_precond=True, pmo_kwargs={}, precond_kwargs={}):
+    import pymanopt as pmo
+    K = jac_basis.shape[1]
+    manifold = pmo.manifolds.grassmann.Grassmann(K, m)
+    @pmo.function.numpy(manifold)
+    def cost(G): return poincare_loss_bigoni(G, jac_u, jac_basis)
+
+    @pmo.function.numpy(manifold)
+    def euclidean_gradient(G): return poincare_loss_bigoni_gradient(G, jac_u, jac_basis)
+
+    precond = None
+    if use_precond:
+        def precond(G, x): return eval_SGinv_X(G, x, jac_u, jac_basis, **precond_kwargs)
+
+    problem = pmo.Problem(manifold, cost, euclidean_gradient=euclidean_gradient, preconditioner=precond)
+    # line_search = pmo.optimizers.line_search.AdaptiveLineSearcher(max_iterations=50) # can be provided to optimizer
+    optimizer = pmo.optimizers.conjugate_gradient.ConjugateGradient(**pmo_kwargs, line_searcher=None)
+
+    return problem, optimizer
+
---
+++ tensap/cofnet/tests/tests_poincare_loss_learning.py
@@ -0,0 +1,80 @@
+
+import numpy as np
+import tensap
+from tensap.cofnet.poincare_loss_learning import *
+
+
+def _build_test_case(d=5, n=2, N=50, m=2):
+    # Build a multivariate polynomial basis with total degree at most 2
+    x = np.random.RandomState(0).uniform(-1, 1, size=(N, d))
+    h = tensap.PolynomialFunctionalBasis(tensap.LegendrePolynomials(), range(3))
+    H = tensap.FunctionalBases.duplicate(h, d)
+    I0 = tensap.MultiIndices.with_bounded_norm(d, 1, 2).remove_indices(0)
+    basis = tensap.SparseTensorProductFunctionalBasis(H, I0)
+    K = basis.eval(x).shape[1]
+
+    G0 = np.linalg.svd(np.array(np.random.RandomState(0).normal(scale=1 / np.sqrt(K), size=(K, m))), full_matrices=False)[0]
+    G1 = np.array(np.random.RandomState(1).normal(scale=1 / np.sqrt(K), size=(K, m)))
+    G2 = np.array(np.random.RandomState(2).normal(scale=1 / np.sqrt(K), size=(K, m)))
+
+    def g(x):
+        return basis.eval(x) @ G0
+
+    def jac_g(x):
+        jac_basis = basis.eval_jacobian(x)
+        out = np.array([G0.T @ jb for jb in jac_basis])
+        return out
+
+    def f(z):
+        out = np.block([[np.sin(z.prod(axis=1))], [np.cos(z).prod(axis=1)]]).T
+        return out
+
+    def jac_f(z):
+        out = np.zeros((z.shape[0], n, z.shape[1]))
+        out[:,0, :] = np.array([np.cos(zi.prod()) * zi.prod() * np.ones(zi.shape[0]) / zi for zi in z])
+        out[:,1, :] = np.array([- np.cos(zi).prod() * np.ones(zi.shape[0]) * np.sin(zi) / np.cos(zi) for zi in z])
+        return out
+
+    def u(x):
+        if x.ndim == 1:
+            x = x.reshape(1, -1)
+        return f(g(x))
+
+    def jac_u(x):
+        if x.ndim == 1:
+            x = x.reshape(-1)
+        jac_fz = jac_f(g(x))
+        jac_gx = jac_g(x)
+        out = np.array([jf @ jg for jf, jg in zip(jac_fz, jac_gx)])
+        return out
+
+    loss = tensap.PoincareLossBigoni(jac_u(x), basis.eval_jacobian(x), basis)
+    loss1 = tensap.PoincareLossBigoni(jac_u(x)[:, [0], :], basis.eval_jacobian(x), basis)
+    loss2 = tensap.PoincareLossBigoni(jac_u(x)[:, [1], :], basis.eval_jacobian(x), basis)
+    resolution = np.sqrt(np.finfo(G0.dtype).resolution)
+
+    return G0, G1, G2, loss, loss1, loss2, resolution
+
+def test_minimize_qn_bigoni():
+    G_true, G1, _, loss, _, _, resolution = _build_test_case(m=1)
+    G_fit = minimize_qn_bigoni(G1, loss.jac_u, loss.jac_basis, maxiter_qn=100, tol_qn=1e-7, verbosity=0)
+    sigma = np.linalg.svd(G_fit.T @ G_true)[1].min()
+    np.testing.assert_allclose(sigma, 1.)
+
+def test_minimize_scipy():
+    G_true, G1, _, loss, _, _, resolution = _build_test_case(m=1)
+    G_fit = minimize_scipy(G1, loss.jac_u, loss.jac_basis, method="Newton-CG", verbosity=0)
+    sigma = np.linalg.svd(G_fit.T @ G_true)[1].min()
+    np.testing.assert_allclose(sigma, 1.)
+
+def test_minimize_pymanopt():
+    G_true, G1, _, loss, _, _, resolution = _build_test_case(m=1)
+    G_fit = minimize_pymanopt(G1, loss.jac_u, loss.jac_basis, use_precond=True, pmo_kwargs={"max_iterations":50, "verbosity":0})
+    sigma = np.linalg.svd(G_fit.T @ G_true)[1].min()
+    np.testing.assert_allclose(sigma, 1.)
+
+
+if __name__ == "__main__":
+    test_minimize_qn_bigoni()
+    test_minimize_scipy()
+    test_minimize_pymanopt()
