81427f76441985529ec70428ddc922eae33dac0e
alexandre-pasco <alexandre.pasco1702@gmail.com>
2024-07-10 09:42:31 +0200

    Removed augmented loss function.

--- tensap/cofnet/poincare_loss_bigoni.py
+++ tensap/cofnet/poincare_loss_bigoni.py
@@ -63,38 +63,6 @@ def poincare_loss_bigoni(G, jac_u, jac_basis, jac_g=None):
     out = poincare_loss(jac_u, jac_g)
     return out
 
-def poincare_loss_bigoni_augmented(G, jac_u, jac_basis, jac_g=None, alpha=1):
-    """
-    Evaluate the Poincare based loss for the feature map G.T @ basis, as described in Bigoni et al. 2022,
-    with dimension augmentation as described in Verdiere et al. 2023.
-
-    Parameters
-    ----------
-    jac_u : numpy.ndarray
-        Samples of the jacobian of the function to approximate.
-        jac_u[k,i,j] is du_i / dx_j evaluated at the k-th sample.
-        Has shape (N, n, d).
-    jac_g : numpy.ndarray
-        Samples of the jacobian of the feature map, whose quality is assessed by the Poincare based loss.
-        jac_g[k,i,j] is dg_i / dx_j evaluated at the k-th sample.
-        Has shape (N, m, d).
-    jac_g : numpy.ndarray, optional
-        Samples of the jacobian of the feature map, whose quality is assessed by the Poincare based loss.
-        jac_g[k,i,j] is dg_i / dx_j evaluated at the k-th sample. If not provided, it is computed from G and jac_basis.
-        Has shape (N, m, d).
-    alpha : float
-        The regularisation parameter
-    Returns
-    -------
-    out : float
-        Poincare loss.
-    """
-    if jac_g is None:
-        jac_g = eval_jac_g(G, jac_basis)
-    out = poincare_loss_bigoni_augmented(jac_u, jac_g, alpha)
-    return out
-
-
 def poincare_loss_bigoni_gradient(G, jac_u, jac_basis, jac_g=None):
     """
     Compute the gradient of the Poincaré-based of G.T @ basis with respect to G.
